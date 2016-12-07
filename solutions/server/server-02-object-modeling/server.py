d8748bb1447592b27dfd4e76e08aa500710db6d5
Michael Fröhlich <mike@Michaels-Air-3.fritz.box>
2016-12-07 12:43:59 +0100

    Add solution code

--- solutions/server/server-02-object-modeling/server.py
+++ solutions/server/server-02-object-modeling/server.py
@@ -4,9 +4,19 @@
 from flask import Flask, send_file
 import sys
 
+from task import Task
+from list import List
+
 # allow special characters (e.g. üäö ...)
 reload(sys)
 sys.setdefaultencoding('utf-8')
+
+myList = List('Inbox', id='0')
+myList.tasks = [
+    Task('Think about lunch', '0', id='0', status = Task.COMPLETED),
+    Task('Become a pro in backend development', '0', status= Task.NORMAL),
+    Task('CONQUER THE WORLD!', '0', status = Task.NORMAL)
+]
 
 # Note: Setting static_url_path to '' has the following effect:
 #   - Whenever a file is requested and there is no matching route defined
